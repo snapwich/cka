b036149df040732cffd93d8e3ffc9422d08ca3a9
Rich Snapp <rich.snapp@gmail.com>
2024-05-07 19:37:02 -0600

    add dns records for nodes

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ provider "google" {
 }
 
 resource "google_compute_network" "vpc_network" {
-  name                    = "k8s"
+  name                    = "k8s-cka"
   auto_create_subnetworks = false
 }
 
@@ -16,6 +16,95 @@ resource "google_compute_subnetwork" "vpc_subnetwork" {
   region        = "us-west1"
   network       = google_compute_network.vpc_network.self_link
 }
+
+resource "google_compute_firewall" "allow_ssh_jumpbox" {
+  name    = "allow-ssh-jumpbox"
+  network = google_compute_network.vpc_network.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["166.70.229.151/32"]  // Allows SSH access from my IP address
+
+  target_tags = ["ssh"]
+}
+
+resource "google_dns_managed_zone" "k8s-cka" {
+  name        = "internal"
+  dns_name    = "internal."
+  description = "Private zone for internal DNS"
+  visibility  = "private"
+
+  private_visibility_config {
+    networks {
+      network_url = google_compute_network.vpc_network.self_link
+    }
+  }
+}
+
+resource "google_dns_record_set" "k8s_nodes" {
+  count        = length(google_compute_instance.k8s-node)
+  name         = "${google_compute_instance.k8s-node[count.index].name}.internal."
+  type         = "A"
+  ttl          = 300
+  managed_zone = google_dns_managed_zone.k8s-cka.name
+  rrdatas      = [google_compute_instance.k8s-node[count.index].network_interface[0].network_ip]
+}
+
+resource "google_compute_instance" "jumpbox" {
+  boot_disk {
+    auto_delete = true
+    device_name = "jumpbox"
+
+    initialize_params {
+      image = "projects/debian-cloud/global/images/debian-12-bookworm-v20240415"
+      size  = 10
+      type  = "pd-balanced"
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = true
+  deletion_protection = false
+  enable_display      = false
+
+  labels = {
+    goog-ec-src = "vm_add-tf"
+  }
+
+  machine_type = "e2-small"
+  name         = "jumpbox"
+
+  network_interface {
+    access_config {
+      network_tier = "STANDARD"
+    }
+
+    queue_count = 0
+    stack_type  = "IPV4_ONLY"
+    subnetwork  = google_compute_subnetwork.vpc_subnetwork.self_link
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  tags = ["ssh"]
+  zone = "us-west1-c"
+}
+
 
 resource "google_compute_instance" "k8s-node" {
   count = 1
@@ -45,10 +134,6 @@ resource "google_compute_instance" "k8s-node" {
   name         = "k8s-node-${count.index}"
 
   network_interface {
-    access_config {
-      network_tier = "STANDARD"
-    }
-
     queue_count = 0
     stack_type  = "IPV4_ONLY"
     subnetwork  = google_compute_subnetwork.vpc_subnetwork.self_link
