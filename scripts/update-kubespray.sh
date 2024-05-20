edccc5ae2f5bc998ea1d44a1cfe3e250383c5b28
Rich Snapp <rich.snapp@gmail.com>
2024-05-20 15:02:37 -0600

    add update kubespray script

--- scripts/update-kubespray.sh
+++ scripts/update-kubespray.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
 
+git remote add kubespray git@github.com:kubernetes-sigs/kubespray.git
+git fetch kubespray master
+git subtree pull --prefix ./kubespray kubespray master --squash
